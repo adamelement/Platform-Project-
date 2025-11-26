3a2ea0ea0efe6f19a2125745f65fc67abf4e2317
koolkid41 <sullivanb@elementhighschool.com>
2025-11-26 10:22:52 -0500

    moveset dictionary along with base character class

--- character.py
+++ character.py
@@ -1,1 +1,39 @@
 
+class Character(self, jump_height, movement_speed, weight, lives, direction):
+    def __init__():
+        self.jump_height = jump_height
+        self.movement_speed = movement_speed
+        self.weight = weight
+        self.lives = lives
+        self.direction = direction
+
+
+marshal = Character( 31 , 1.8 , 95  )
+    
+
+
+marshals_moves = {'ftilt': {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'jab' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'up_tilt': {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': },
+                     'down_tilt' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': },
+                     'dash_attack' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'nair' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'fair' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'bair' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'dair' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'up_air' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'f_smash' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'up_smash' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'down_smash' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'neutral_b' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'up_b' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }
+                     'down_b' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'side_b' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'grab' : {'damage' 0 :  , 'knock_back' 0 : '', 'launch_angle': 0 , 'knockback_scaling' 0 : , 'hitox' :  , 'startup' : , 'endlag': }, 
+                     'up_throw' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'down_throw' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'back_throw' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'forward_throw' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'attack_getup' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }, 
+                     'ledge_attack' : {'damage' :  , 'knock_back' : '', 'launch_angle': , 'knockback_scaling' : , 'hitox' :  , 'startup': , 'endlag': }}, 
+
