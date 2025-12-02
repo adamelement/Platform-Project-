c1c44a60829df2e36a8aa325c9b59307ae9c4f1a
koolkid41 <sullivanb@elementhighschool.com>
2025-12-02 15:35:14 -0500

    input handler skeleton

--- input_handler.py
+++ input_handler.py
@@ -1,1 +1,17 @@
+class InputHandler():
 
+    def jump():
+
+    def attack():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+        elif keys[pygame/K_a]:      
+        else:
+
+    def special_attack():
+
+    def movement():
+
+    def shielding():
+
+    def 
