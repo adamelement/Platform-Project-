79d667a00c432054b7b132946debbfa138188daa
koolkid41 <sullivanb@elementhighschool.com>
2025-12-03 10:06:01 -0500

    fleshed out jump method in input handler

--- input_handler.py
+++ input_handler.py
@@ -2,18 +2,27 @@ class InputHandler():
 
     def jump():
         global air_bound
-        air_bound = True
+        if not air_bound:
+            character.jump()
+            air_bound = True
+            double_jump = False
+        elif air_bound and not double_jump:
+            character.jump()
 
     def attack():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
-        elif keys[pygame.K_LEFT]:      
+        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_UP]:
+        elif keys[pygame.K_DOWN]:      
         else:
 
 
     def special_attack():
         if keys[pygame.K_RIGHT]:
-        elif keys[pygame.K_LEFT]:      
+        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_UP]:
+        elif keys[pygame.K_DOWN]:      
         else:
 
     def movement():
@@ -25,7 +34,9 @@ class InputHandler():
 
     def shielding():
         if keys[pygame.K_RIGHT]:
-        elif keys[pygame.K_LEFT]:      
+        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_UP]:
+        elif keys[pygame.K_DOWN]:      
         else:
 
     def 
