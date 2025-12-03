f80da26e5452aca0a2720bb92416cdd32d46cdc3
koolkid41 <sullivanb@elementhighschool.com>
2025-12-03 09:48:36 -0500

    more skeleton

--- input_handler.py
+++ input_handler.py
@@ -1,17 +1,31 @@
 class InputHandler():
 
     def jump():
+        global air_bound
+        air_bound = True
 
     def attack():
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-        elif keys[pygame/K_a]:      
+        if keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_LEFT]:      
         else:
 
+
     def special_attack():
+        if keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_LEFT]:      
+        else:
 
     def movement():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+
+        elif keys[pygame.K_LEFT]:
+        
 
     def shielding():
+        if keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_LEFT]:      
+        else:
 
     def 
