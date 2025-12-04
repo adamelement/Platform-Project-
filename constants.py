7cbbd2552443bacf0ea9fe8bdccc548832a17524
BingledorfHiggens <evanx@elementhighschool.com>
2025-12-05 04:00:14 +0800

    added constants

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
-print('Live update test2')
-print('Live update test2')
+    SCREEN_WIDTH = 1080
+    SCREEN_HEIGHT = 1080
+    BACKGROUND_COLOUR = (0, 0, 0)
+    CAPTION = Platform Fight!
+    FPS = 60
+    PLAYER1_COLOUR = (0, 0, 255)# will not be shown once sprites are blitted on,
+    PLAYER2_COLOUR = (255, 255, 0) # will not be shown once sprites are present, 
+    HITBOX_COLOUR = (255, 0, 255) # will not be drawn, making these hitboxes effectively invisible
