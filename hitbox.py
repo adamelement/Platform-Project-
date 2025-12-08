7d90834b4c319d8ae1d63955c30f21b68559d1b2
koolkid41 <sullivanb@elementhighschool.com>
2025-12-08 15:03:39 -0500

    some hitbox class stuff

--- hitbox.py
+++ hitbox.py
@@ -1,1 +1,12 @@
+class Hitbox:
+    def __init__(self, hitbox_dimensions):
+        self.character_direction = character_direction
+        self.hitbox_dimensions = hitbox_dimensions
+
+    def attack_hitbox(self):
+        self.rect = pygame.Rect(x, y, self.hitbox[0],self.hitbox[1])
+        direction = Character.character_direction
+
+
+
 
