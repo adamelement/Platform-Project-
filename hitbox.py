30fd4d66af6e8bb578a873e8b2467dbe7a5093a6
koolkid41 <sullivanb@elementhighschool.com>
2025-12-10 10:05:15 -0500

    more

--- hitbox.py
+++ hitbox.py
@@ -6,9 +6,9 @@ class Hitbox:
     def attack_hitbox(self):
         direction = Character.character_direction
         if direction == 'L':
-            self.rect = marshal_moveset['hitbox', 'hitbox', character_location - 'x_distance', character_location - 'y_distance']
+            self.rect = (['hitbox'], ['hitbox'], character_location - ['x_distance'], character_location + ['y_distance'])
         elif direction == 'R':
-            self.rect = marshal_moveset['hitbox', 'hitbox', character_location + 'x_distance', character_location + 'y_distance']
+            self.rect = (['hitbox'], ['hitbox'], character_location + ['x_distance'], character_location + ['y_distance'])
 
 
 
