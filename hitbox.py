1f6787836a5cc3cc1edccdf2079b2119e9c27253
koolkid41 <sullivanb@elementhighschool.com>
2025-12-10 09:57:56 -0500

    some hitbox changes

--- hitbox.py
+++ hitbox.py
@@ -4,8 +4,11 @@ class Hitbox:
         self.hitbox_dimensions = hitbox_dimensions
 
     def attack_hitbox(self):
-        self.rect = pygame.Rect(x, y, self.hitbox_dimensions[0],self.hitbox_dimensions[1])
         direction = Character.character_direction
+        if direction == 'L':
+            self.rect = marshal_moveset['hitbox', 'hitbox', character_location - 'x_distance', character_location - 'y_distance']
+        elif direction == 'R':
+            self.rect = marshal_moveset['hitbox', 'hitbox', character_location + 'x_distance', character_location + 'y_distance']
 
 
 
