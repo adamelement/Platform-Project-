8f7c2b0f6e56441261ad02925844332ec136294b
koolkid41 <sullivanb@elementhighschool.com>
2025-12-11 14:52:23 -0500

    hitbox changes

--- hitbox.py
+++ hitbox.py
@@ -1,15 +1,10 @@
 class Hitbox:
     def __init__(self, dimensions, knockback, launch_angle):
-        self.dimensions = dimensions
+        self.hitbox = pygame.Rect(dimensions)
         self.knockback = knockback
         self.launch_angle = launch_angle
 
-    def attack_hitbox(self):
-        direction = Character.character_direction
-        if direction == 'L':
-            self.rect = (['hitbox'], ['hitbox'], character_location - ['x_distance'], character_location + ['y_distance'])
-        elif direction == 'R':
-            self.rect = (['hitbox'], ['hitbox'], character_location + ['x_distance'], character_location + ['y_distance'])
+   # def attack_hitbox(self):
 
 
 
