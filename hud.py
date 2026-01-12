56f1b5e91d2df296c478164e410d5e7223603e2c
BingledorfHiggens <evanx@elementhighschool.com>
2026-01-13 04:37:18 +0800

    Started Hud

--- hud.py
+++ hud.py
@@ -1,1 +1,9 @@
-
+import pygame()
+class HUD:
+    def __init__(self, percent, lives, position, size)
+        self.percent = pygame.Character(percent)
+        self.lives = pygame.Character(lives)
+        self.position = position
+        self.size = size
+    p1_hud = pygame.image.load('assets/backgrounds/hud_assets/hud.webp').convert_alpha
+    p2_hud = pygame.image.load('assets/backgrounds/hud_assets/hud.webp').convert_alpha
