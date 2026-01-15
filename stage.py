d1eab9218496eed4ef3a4038ea12e846aa72c1ee
koolkid41 <sullivanb@elementhighschool.com>
2026-01-15 15:43:54 -0500

    updated collision

--- stage.py
+++ stage.py
@@ -1,25 +1,27 @@
 import pygame
 pygame.init()
-from character import Character
 class Stage():
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self):
+
         self.stage_top = pygame.Rect(50, 700, 1820, 100)
         self.stage_bottomr = pygame.Rect(0,0,0,0)
         self.stage_bottoml = pygame.Rect(0,0,0,0)
 
-    def ground_collision(self):
-        if self.stage_top.colliderect(self.player1.rect):
-            self.player1.y -= 1 
-            self.player1.airborn = False
-        else:
-            self.player1.airborn = True
-        if self.stage_top.colliderect(self.player2.rect):
-            self.player2.y -= 1 
-            self.player2.airborn = False
-        else:
-            self.player2.airborn = True
+    def ground_collision(self, player):
+        if player.vy >= 0:
+            prev_bottom = player.rect.bottom  - player.vy
+            if player.rect.right > self.stage_top.left and player.rect.left < self.stage_top.right:
+                if prev_bottom <= self.stage_top.top and player.rect.bottom >= self.stage_top.top:
+                    player.rect.bottom = self.stage_top.top
+                    player.vy = 0
+                    player.airborn = False
+                    return
+        player.airborn = True
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (120, 120, 120), self.stage_top)
+
+
 
     def stage_collision(self):
         if self.stage_bottomr.colliderect(self.player1.rect):
@@ -32,5 +34,4 @@ class Stage():
             self.player2.x -= 1
     
 
-    
-
+   
